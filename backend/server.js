7ba9faef7488a0f49c30a05cca8a34df34bf528f
PlaquetWilliam <wplaquet72@gmail.com>
2025-09-30 11:32:43 +0200

    FIX : Problème d'api

--- backend/server.js
+++ backend/server.js
@@ -79,6 +79,6 @@ app.patch('/tasks/:id/completed', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Serveur démarré sur le port ${PORT}`);
 });
